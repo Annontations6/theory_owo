02aa6db212e9c20609a976bc74480f9a3964e4a7
Glitch (irradiated-glitter-enthusiasm) <none>
2022-07-09 03:11:04 +0000

    🐊♻️ Updated with Glitch

--- Theory Automator.js
+++ Theory Automator.js
@@ -12,6 +12,29 @@ var version = "1.0.0";
 
 var init = () => {
   currency = theory.createCurrency();
+  
+  // a1
+    {
+        let getDesc = (level) => "a_1=\\sqrt{" + level + "}";
+        let getInfo = (level) => "a_1=" + getA1(level).toString(0);
+        a1 = theory.createUpgrade(1, currency, new ExponentialCost(5, Math.log2(3)));
+        a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
+        a1.getInfo = (amount) => Utils.getMathTo(getInfo(a1.level), getInfo(a1.level + amount));
+    }
 }
+
+var getPrimaryEquation = () => {
+    let result = "\\dot{\\rho} = a_1^{1.025}a_2";
+
+    return result;
+}
+
+var getSecondaryEquation = () => theory.latexSymbol + "=\\max\\rho^{0.2}";
+var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
+var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getTau = () => currency.value.pow(0.2);
+var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
+
+var getA1 = (level) => BigNumber.from(level).sqrt();
 
 init();
