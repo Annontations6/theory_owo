caa32be1a38691dd5c99ec1821b441c3806ca4fa
Glitch (irradiated-glitter-enthusiasm) <none>
2022-07-09 03:45:17 +0000

    🏵️🌿 Updated with Glitch

--- Theory Automator.js
+++ Theory Automator.js
@@ -12,14 +12,25 @@ var version = "1.0.0";
 
 var init = () => {
   currency = theory.createCurrency();
+  timer = theory.createCurrency("s", "s");
+  
+  timer.value = BigNumber.ZERO
   
   // a1
     {
         let getDesc = (level) => "a_1=" + getA1Factor(c1.level) + "\\sqrt{" + level + "}";
         let getInfo = (level) => "a_1=" + getA1(level).toString(0);
-        a1 = theory.createUpgrade(1, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(3))));
+        a1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(5, Math.log2(3))));
         a1.getDescription = (_) => Utils.getMath(getDesc(a1.level));
         a1.getInfo = (amount) => Utils.getMathTo(getInfo(a1.level), getInfo(a1.level + amount));
+    }
+  // a2
+    {
+        let getDesc = (level) => "a_2=" + getA2Factor(c2.level) + "\\sqrt{" + level + "}";
+        let getInfo = (level) => "a_2=" + getA2(level).toString(0);
+        a2 = theory.createUpgrade(1, currency, new FirstFreeCost(new ExponentialCost(1000, Math.log2(5))));
+        a2.getDescription = (_) => Utils.getMath(getDesc(a2.level));
+        a2.getInfo = (amount) => Utils.getMathTo(getInfo(a2.level), getInfo(a2.level + amount));
     }
   
     /////////////////////
@@ -32,16 +43,28 @@ var init = () => {
         c1.getDescription = (amount) => "$\\uparrow$ $a_1$ factor by 1";
         c1.getInfo = (amount) => "$\\uparrow$ $a_1$ factor by 1";
     }
-    theory.createAutoBuyerUpgrade(3, currency, 1e35);
+    //c2
+      {
+        c2 = theory.createPermanentUpgrade(3, currency, new ExponentialCost(1e7, Math.log2(10)));
+        c2.getDescription = (amount) => "$\\uparrow$ $a_2$ factor by 1";
+        c2.getInfo = (amount) => "$\\uparrow$ $a_2$ factor by 1";
+    }
+    theory.createAutoBuyerUpgrade(4, currency, 1e35);
   
     ///////////////////////
     //// Milestone Upgrades
     theory.setMilestoneCost(new LinearCost(2, 2));
+  
+    /////////////////
+    //// Achievements
+    let achievement_category_1 = theory.createAchievementCategory(0, "Timer");
+    achievement1 = theory.createAchievement(0, achievement_category_1, "You Played!", "I Think now.", () => timer.value > 1);
 }
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
+    timer.value += BigNumber.from(0.1)
     currency.value += dt * bonus * getA1(a1.level).pow(1.025)
 }
 
